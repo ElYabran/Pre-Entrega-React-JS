8a61b482fc18e4e4bd970565f701116d7d7a8885
Brian <pitzbrian97@gmail.com>
2025-10-28 08:47:21 -0300

    Configuración del package.json para deploy en vercel

--- talentolab-ecommerce/vite.config.js
+++ talentolab-ecommerce/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 })
